448dc80358e3474a805d3d59937bc13e8cc573d4
Goutham Reddy Kotapalle <goutamkreddy@gmail.com>
2020-04-27 06:25:32 -0500

    existing func check

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,8 +1,41 @@
-import boto3
 import json
 
-ec2 = boto3.client('ec2')
+from datetime import datetime
+from copy import deepcopy
+import json
+import boto3
+import uuid
+import sys
+
+
 def lambda_handler(event, context):
-    print("this is an update")
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+    # TODO implement
+
+    conn = boto3.client("emr")
+
+    clusters = conn.list_clusters()
+    # choose the correct cluster
+    clusters = [c["Id"] for c in clusters["Clusters"]
+                if c["Status"]["State"] in ["RUNNING", "WAITING"]]
+    if not clusters:
+        sys.stderr.write("No valid clusters\n")
+        sys.stderr.exit()
+    cluster_id = clusters[0]
+    print(cluster_id)
+    # code location on youra emr master node
+    CODE_DIR = "/home/hadoop/"
+
+    # spark configuration example
+    step_args = ["/usr/bin/spark-submit", "", "",
+                 CODE_DIR + "wordcount.py", '', '']
+
+    step = {"Name": "ProcessWordCount",
+            'ActionOnFailure': 'CONTINUE',
+            'HadoopJarStep': {
+                'Jar': 's3n://elasticmapreduce/libs/script-runner/script-runner.jar',
+                'Args': step_args
+            }
+            }
+    action = conn.add_job_flow_steps(JobFlowId=cluster_id, Steps=[step])
+    return "Added step: %s" % (action)
+
